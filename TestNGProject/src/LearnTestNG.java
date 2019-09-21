efa74a68d61d8a0de71d7e6c21a5dcdf6b430858
Utkarsh Honparkhe <honparkhe01utkarsh@gmail.com>
2019-09-21 21:11:44 +0530

    My first commit to publish repo

---
+++ TestNGProject/src/LearnTestNG.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class LearnTestNG {
+
+	@Test
+	public void Login() {
+	System.out.println("Login");
+		
+	}
+	
+	@Test
+	public void Logout() {
+	System.out.println("Logout");
+		
+	}
+	
+	@Test
+	public void navigate() {
+	System.out.println("Navigate");
+		
+	}
+}
